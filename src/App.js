8b5e6e12c59d1aca3fc6b40a9cb7900972b9e585
YihuiTham <yihuitham@gmail.com>
2021-12-20 11:12:36 +0800

    install mui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component='form'
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete='off'
+    >
+      <TextField id='outlined-basic' label='Outlined' variant='outlined' />
+      <TextField id='filled-basic' label='Filled' variant='filled' />
+      <TextField id='standard-basic' label='Standard' variant='standard' />
+    </Box>
   );
 }
-
-export default App;
