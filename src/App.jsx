2214b94deb7a1319c2ec72adb5b8c36aec3fd44c
kienwei96 <kien_wei@hotmail.com>
2021-12-24 13:45:57 +0800

    create private route to protect API route by verify the access token validity

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
 import Login from './pages/Login';
 import StaffDashboard from './pages/StaffDashboard';
 import ChiefDashboard from './pages/ChiefDashboard';
 import { AuthProvider, AuthContext } from './context/AuthContext';
 import { FetchProvider } from './context/FetchContext';
 
+const PrivateRoute = () => {
+  const authContext = useContext(AuthContext);
+  return authContext.isAuthenticated() ? <Outlet /> : <Navigate to='/' />;
+};
+
+const AdminRoute = () => {
+  const authContext = useContext(AuthContext);
+  return authContext.isAuthenticated() && authContext.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/' />
+  );
+};
+
 const AppRoutes = () => {
+  const authContext = useContext(AuthContext);
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/chief' element={<ChiefDashboard />} />
-        <Route path='/dashboard' element={<StaffDashboard />} />
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/chief' element={<PrivateRoute />}>
+          <Route exact path='/chief' element={<ChiefDashboard />} />
+        </Route>
+        <Route exact path='/dashboard' element={<AdminRoute />}>
+          <Route exact path='/dashboard' element={<StaffDashboard />} />
+        </Route>
       </Routes>
     </>
   );
