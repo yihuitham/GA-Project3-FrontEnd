acce0d205f3ead4951ef92a2554d6f69facb39a6
kienwei96 <kien_wei@hotmail.com>
2021-12-31 13:32:02 +0800

    lift state from StaffDashboard.jsx and props to currect state

--- src/components/OperationDetails.jsx
+++ src/components/OperationDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   TextField,
@@ -17,7 +17,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-export default function OperationDetails() {
+export default function OperationDetails(props) {
+  const [data, setData] = useState(null);
+  const [nurses, setNurses] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setData(props.operationData.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const getNurses = async () => {
+      try {
+        const nursesList = [];
+        for (let i = 0; i < data.nursesID.length; i++) {
+          nursesList.push(data.nursesID[i].name);
+        }
+        const nurses = nursesList.join();
+        setNurses(nurses);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getData();
+    getNurses();
+  }, [props]);
+
+  console.log('operationData', data);
+
   return (
     <Paper
       sx={{
@@ -42,32 +73,42 @@ export default function OperationDetails() {
           <TableRow>
             <TableCell align='center'> Operation Details </TableCell>
             <TableCell align='left'>Operation Theatre </TableCell>
-            <TableCell align='left'> 1 </TableCell>
+            <TableCell align='left'>
+              {data === null ? ' ' : data.operatingRoom}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align='center'> </TableCell>
             <TableCell align='left'>Operation Date </TableCell>
-            <TableCell align='left'> 5 January 2022 </TableCell>
+            <TableCell align='left'>
+              {data === null ? ' ' : data.date}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align='center'> </TableCell>
             <TableCell align='left'>Operation Time </TableCell>
-            <TableCell align='left'> 14:00 </TableCell>
+            <TableCell align='left'>
+              {data === null ? ' ' : data.time}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align='center'> </TableCell>
             <TableCell align='left'>Operation Description </TableCell>
-            <TableCell align='left'> Heart Surgery </TableCell>
+            <TableCell align='left'>
+              {data === null ? ' ' : data.operation}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align='center'> </TableCell>
             <TableCell align='left'>Surgeon </TableCell>
-            <TableCell align='left'> Wei Lun </TableCell>
+            <TableCell align='left'>
+              {data === null ? ' ' : data.surgeonID.name}
+            </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align='center'> </TableCell>
             <TableCell align='left'>Nurses </TableCell>
-            <TableCell align='left'> Siew La </TableCell>
+            <TableCell align='left'>{data === null ? ' ' : nurses}</TableCell>
           </TableRow>
         </Table>
       </TableContainer>
